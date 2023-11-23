366da3eaa0d913919bf284f80bd046efb0f9bd18
drktao <83025663+drktao@users.noreply.github.com>
2023-11-23 03:34:28 +0000

    db notebooks

--- mylib/query.py
+++ mylib/query.py
@@ -7,26 +7,27 @@
 
 
 # Usage of Spark SQL for data transformations
-Num_songs_year = spark.sql("""
+avg_duration_artist = spark.sql("""
     SELECT
-    year,
-    count(artist_name) AS num_songs
+    artist_name,
+    avg(duration) AS avg_duration
     FROM
-    prepare_songs_data
+    prepared_data
     WHERE
-    year > 0
+    year>0
     GROUP BY
-    year
+    artist_name
     ORDER BY
-    year DESC
+    avg_duration DESC
+    LIMIT 20
 """).toPandas()
 
-Num_songs_year
+avg_duration_artist
 
 # COMMAND ----------
 
-# Proper error handling
-row = Num_songs_year.count()[1]
+# Data Validation Check
+row = avg_duration_artist.count()[1]
 if row>0:
     print(f"Data validation passed. {row} rows available.")
 else:
@@ -35,32 +36,25 @@ else:
 # COMMAND ----------
 
 # Visualization of the transformed data
-# Plot a bar plot
 plt.figure(figsize=(15, 8))
-plt.bar(Num_songs_year["year"], Num_songs_year["num_songs"], color='skyblue')
-plt.title("Number of songs for Each Year")
-plt.xlabel("Year")
-plt.ylabel("Number of songs")
+plt.bar(avg_duration_artist["artist_name"], avg_duration_artist["avg_duration"], color='skyblue')
+plt.title("Top 20 Artists by Average Song Duration")
+plt.xlabel("Artist")
+plt.ylabel("Average Duration")
+plt.xticks(rotation=90)
 plt.show()
-# Save the figure
-plot_path = "/dbfs/FileStore/IDS706_Data_Pipeline/num_songs_per_year.png"
-plt.savefig(plot_path)
-plt.close()
 
 
 # COMMAND ----------
 
 # Spark SQL Query: Which artists released the most songs each year?
-top_artists = spark.sql(
-    """
+top_artists=spark.sql("""
     SELECT
     artist_name,
-    count(artist_name)
-    AS
-    num_songs,
+    count(artist_name) AS num_songs,
     year
     FROM
-    prepare_songs_data
+    prepared_data
     WHERE
     year > 0
     GROUP BY
@@ -69,27 +63,26 @@ top_artists = spark.sql(
     ORDER BY
     num_songs DESC,
     year DESC
-    LIMIT 10
-"""
-)
+    LIMIT 5;
+""").toPandas()
+
+top_artists
 
 # COMMAND ----------
 
 # Spark SQL Query: Find songs for your DJ list
-top_DJ = spark.sql(
-    """
+top_tempo=spark.sql(""" 
     SELECT
     artist_name,
     title,
     tempo
     FROM
-    prepare_songs_data
+    prepared_data
     WHERE
     time_signature = 4
     AND
     tempo between 100 and 140
-    ORDER BY tempo DESC
-    LIMIT 10
-"""
-)
+    LIMIT 5;
+""").toPandas()
 
+top_tempo
